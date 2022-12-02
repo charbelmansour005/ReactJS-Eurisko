fc93cc8811a7cdca3aeddcd5fab7d09b809d974a
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-03 01:23:59 +0200

    redux >>

--- src/features/article/articleActions.js
+++ src/features/article/articleActions.js
@@ -1,23 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { BASE_URL } from "../../helpers/url";
-// import { useSelector } from "react-redux";
 
-// export const fetchArticles = createAsyncThunk(
-//   "article/fetchArticles",
-//   async () => {
-//     const response = await axios.get(`${BASE_URL}/articles?page=1`);
-//     return response.data.response.docs;
-//   }
-// );
-
-// CONTINUE WORKING ON THIS
 export const fetchArticles = createAsyncThunk(
   "article/fetchArticles",
   async (arg, { getState }) => {
     const page = getState().article.page;
     console.log(page);
-    // const page = useSelector((state) => state.article.page);
     const response = await axios.get(`${BASE_URL}/articles?page=${page}`);
     return response.data.response.docs;
   }
