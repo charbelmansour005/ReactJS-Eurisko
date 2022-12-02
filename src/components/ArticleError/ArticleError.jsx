5d3bcd51898f65631027eff3c0684cd00f18f366
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-02 22:17:46 +0200

    un-needed code removed

--- src/components/ArticleError/ArticleError.jsx
+++ src/components/ArticleError/ArticleError.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import classes from "./ArticleError.module.css";
--- src/features/article/articleActions.js
+++ src/features/article/articleActions.js
@@ -5,12 +5,6 @@ import { BASE_URL } from "../../helpers/url";
 export const fetchArticles = createAsyncThunk(
   "article/fetchArticles",
   async () => {
-    // const accessToken = localStorage.getItem("userToken");
-    // const config = {
-    //   headers: { Authorization: `Bearer ${accessToken}` },
-    // };
-    // const page = useSelector((state) => state.article.page);
-    // const response = await axios.get(`${BASE_URL}/articles?page=1`, config);
     const response = await axios.get(`${BASE_URL}/articles?page=1`);
     return response.data.response.docs;
   }
