6cc3c77560cb75cc6e2eaace0fbcb76190d9cce7
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-07 01:11:35 +0200

    Electricity came... final minor edit

--- src/components/DashNoSearchRes/DashNoSearchRes.jsx
+++ src/components/DashNoSearchRes/DashNoSearchRes.jsx
@@ -4,9 +4,7 @@ import classes from "./DashNoSearchRes.module.css";
 const DashNoSearchRes = () => {
   return (
     <div className={classes.condensed}>
-      <Alert severity="info">
-        If you didn't find what you were looking for, try again!
-      </Alert>
+      <Alert severity="info">Cannot find any articles...</Alert>
     </div>
   );
 };
